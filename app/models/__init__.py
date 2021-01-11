6461dbd4f1223ff404dc47c8edea61c5a0a39d50
heunyam <tpwns072@gmail.com>
2021-01-11 22:04:56 +0900

    feat: db session 생성

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from config.db import LocalDBConfig
+
+Base = declarative_base()
+
+db_engine = create_engine(LocalDBConfig.DB_URL)
+Session = scoped_session(sessionmaker(bind=db_engine, autocommit=False, autoflush=False))
+session = Session()
