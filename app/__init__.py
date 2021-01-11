494295a2fb881b7f59a8986c25b1bb0eeefc2929
heunyam <tpwns072@gmail.com>
2021-01-11 22:07:57 +0900

    feat: socketio app 생성

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from app.router import bp
+from flask_jwt_extended import JWTManager
 
 
 def create_app(*config_cls):
 
     _app = Flask(__name__)
-    # _app.register_blueprint()
+    _app.register_blueprint(bp)
 
     for config in config_cls:
         _app.config.from_object(config)
 
+    JWTManager(_app)
+
     return _app
+
+
+def create_socketio(app):
+    socketio = SocketIO(app)
+
+    return socketio
