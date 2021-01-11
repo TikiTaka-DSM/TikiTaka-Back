4ca1c279ee8df5d1ba904129832e424eb265a986
heunyam <tpwns072@gmail.com>
2021-01-11 22:11:08 +0900

    feat: ping api routing setting

--- app/router.py
+++ app/router.py
@@ -1,6 +1,10 @@
 from flask_restful import Api
 from flask import Blueprint
 
+from app.views.ping import Ping
+
 
 bp = Blueprint("api", __name__, url_prefix="")
 api = Api(bp)
+
+api.add_resource(Ping, '/ping')
