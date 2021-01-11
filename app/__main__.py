f7df1e62b8d3d93cdcd5057a0a544e7c15fe3d6e
heunyam <tpwns072@gmail.com>
2021-01-11 22:09:04 +0900

    feat: socketio run setting

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,10 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/ping')
-def ping():
-    return 'pong'
+from app import create_app, create_socketio
+from config.app import LocalAppConfig
+from config.db import LocalDBConfig
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app(LocalAppConfig, LocalDBConfig)
+    socketio = create_socketio(app)
+
+    socketio.run(app, debug=True, host='0.0.0.0')
