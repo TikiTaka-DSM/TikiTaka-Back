4a55c7e5b60e0b06388a02918042fdebf7b2195a
heunyam <tpwns072@gmail.com>
2021-01-15 21:23:09 +0900

    feat: Add user routing

--- app/router.py
+++ app/router.py
@@ -2,9 +2,13 @@ from flask_restful import Api
 from flask import Blueprint
 
 from app.views.ping import Ping
+from app.views.user import Auth, User
 
 
 bp = Blueprint("api", __name__, url_prefix="")
 api = Api(bp)
 
 api.add_resource(Ping, '/ping')
+
+api.add_resource(User, '/user')
+api.add_resource(Auth, '/user/auth')
