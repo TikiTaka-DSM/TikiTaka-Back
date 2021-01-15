26e20e4388811a1ac8e5b49ed8f138c684e5cefc
heunyam <tpwns072@gmail.com>
2021-01-16 00:33:20 +0900

    feat: remote db config

--- config/db.py
+++ config/db.py
@@ -1,6 +1,13 @@
+from config.config import DB_NAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD
+
+
 class LocalDBConfig:
     DB_URL = f'mysql+pymysql://root:heunyam@localhost:3306/tikitaka?charset=utf8mb4'
 
 
 class RemoteDBConfig:
-    pass
+    DATABASE_USER = DB_USER
+    DATABASE_PASSWORD = DB_PASSWORD
+    DATABASE_HOST = DB_HOST
+    DATABASE_PORT = DB_PORT
+    DATABASE_NAME = DB_NAME
