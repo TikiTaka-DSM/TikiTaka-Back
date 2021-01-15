098b7eafff4b35df6541450e8d110b52e22119f4
heunyam <tpwns072@gmail.com>
2021-01-16 00:37:08 +0900

    Change app config to remote db config

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from config.db import LocalDBConfig
+from config.db import RemoteDBConfig
 
 Base = declarative_base()
 
-db_engine = create_engine(LocalDBConfig.DB_URL, echo=False)
+db_engine = create_engine(RemoteDBConfig.DB_URL, echo=False)
 Session = scoped_session(sessionmaker(bind=db_engine, autocommit=False, autoflush=False))
 session = Session()
