d3f5ff6cc61c33ecbe4c6f54423495e469ea4f0f
heunyam <tpwns072@gmail.com>
2021-01-18 23:18:08 +0900

    feat: Add CreateNewRoom view class

--- app/views/room.py
+++ app/views/room.py
@@ -1,12 +1,19 @@
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from app.controllers.room import create_new_chatting_room
 
 
 class Room(Resource):
     def get(self):
         pass
 
+
+class CreateNewRoom(Resource):
+    @jwt_required
     def post(self):
         users = request.json['people']
+        owner_user = get_jwt_identity()
 
-        pass
+        return create_new_chatting_room(owner_user, users)
