50d388578dc47768a33308936d277a30fd405d72
heunyam <tpwns072@gmail.com>
2021-01-18 21:50:37 +0900

    refactor: Add function naming "_"

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,6 +1,6 @@
 from flask import abort
-from flask_jwt_extended import create_access_token, create_refresh_token
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash
 from sqlalchemy.exc import SQLAlchemyError
 
 from app.models import session
@@ -12,7 +12,7 @@ def get_user_data_by_user_id(user_id):
     return session.query(User).filter(User.id == user_id).first()
 
 
-def create_user(user_id, user_password, user_name):
+def _create_user(user_id, user_password, user_name):
 
     user = User(id=user_id,
                 password=generate_password_hash(user_password),
@@ -29,7 +29,7 @@ def sign_up(user_id, user_password, user_name):
         abort(409, "This id is already signed up")
 
     try:
-        create_user(user_id, user_password, user_name)
+        _create_user(user_id, user_password, user_name)
 
     except SQLAlchemyError as e:
         print("[ERROR MESSAGE] " + str(e))
