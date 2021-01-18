3cbf38729484fac5e7a4ed475180210912c0803f
heunyam <tpwns072@gmail.com>
2021-01-18 21:48:14 +0900

    fix: wrong naming "room_id" to "friend_user_id"

--- app/models/friend.py
+++ app/models/friend.py
@@ -7,5 +7,5 @@ class Friend(Base):
     __tablename__ = 'friends'
 
     user_id = Column(String(45), ForeignKey('users.id'), primary_key=True)
-    room_id = Column(Integer, ForeignKey('rooms.id'), primary_key=True)
+    friend_user_id = Column(String(45), ForeignKey('users.id'), primary_key=True)
     blocking_state = Column(Boolean, nullable=False)
