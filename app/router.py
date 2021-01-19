bde48e50c137d88b8224c8ec4a3b867c1867d879
heunyam <tpwns072@gmail.com>
2021-01-19 16:55:53 +0900

    feat: add friend api and get friend list api routing

--- app/router.py
+++ app/router.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 from app.views.ping import Ping
 from app.views.user import Auth, User
+from app.views.friend import AddFriend, GetFriends
 
 
 bp = Blueprint("api", __name__, url_prefix="")
@@ -12,3 +13,5 @@ api.add_resource(Ping, '/ping')
 
 api.add_resource(User, '/user')
 api.add_resource(Auth, '/user/auth')
+api.add_resource(AddFriend, '/friend/<user_id>')
+api.add_resource(GetFriends, '/friends')
