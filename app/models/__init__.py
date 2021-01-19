b7ff306969dfe7d924214a542edf7d3b132b3642
heunyam <tpwns072@gmail.com>
2021-01-19 23:21:14 +0900

    feat: s3 setting

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
+import boto3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,3 +11,8 @@ db_engine = create_engine(RemoteDBConfig.DB_URL, encoding="utf-8", pool_size=20,
 Base = declarative_base()
 Session = scoped_session(sessionmaker(bind=db_engine, autocommit=False, autoflush=False))
 session = Session()
+
+s3 = boto3.client(service_name='s3',
+                  aws_access_key_id=RemoteDBConfig.S3_ACCESS_KEY,
+                  aws_secret_access_key=RemoteDBConfig.S3_SECRET_ACCESS,
+                  region_name=RemoteDBConfig.S3_REGION)
