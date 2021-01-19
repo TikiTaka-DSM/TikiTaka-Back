0f3a945fd378c208ab8e0a0c59f98d420e1ea980
heunyam <tpwns072@gmail.com>
2021-01-19 16:53:58 +0900

    fix: engine option

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from config.db import RemoteDBConfig
 
-Base = declarative_base()
+db_engine = create_engine(RemoteDBConfig.DB_URL, encoding="utf-8", pool_size=20,
+                          pool_recycle=3600, max_overflow=20, pool_pre_ping=True)
 
-db_engine = create_engine(RemoteDBConfig.DB_URL, echo=False)
+Base = declarative_base()
 Session = scoped_session(sessionmaker(bind=db_engine, autocommit=False, autoflush=False))
 session = Session()
