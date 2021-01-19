ba8d70a93deaa661755b6ef07f1debf8aef51502
heunyam <tpwns072@gmail.com>
2021-01-19 16:56:46 +0900

    feat: create_all(engine)

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,6 @@
 from app import create_app, create_socketio
+from app.models import Base, db_engine
+
 from config.app import LocalAppConfig
 from config.db import RemoteDBConfig
 
@@ -6,5 +8,7 @@ from config.db import RemoteDBConfig
 if __name__ == '__main__':
     app = create_app(LocalAppConfig, RemoteDBConfig)
     socketio = create_socketio(app)
+
+    Base.metadata.create_all(db_engine)
 
     socketio.run(app, debug=True)
