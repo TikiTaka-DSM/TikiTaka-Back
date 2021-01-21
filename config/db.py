77c2d9db68acd858ac47f51c7c380a395ee3bdad
heunyam <tpwns072@gmail.com>
2021-01-21 16:38:14 +0900

    feat: Add S3 bucket name

--- config/db.py
+++ config/db.py
@@ -6,7 +6,8 @@ from config.config import (
     DB_PASSWORD,
     S3_REGION,
     S3_ACCESS_KEY,
-    S3_SECRET_ACCESS
+    S3_SECRET_ACCESS,
+    S3_BUCKET_NAME
 )
 
 
@@ -18,4 +19,5 @@ class RemoteDBConfig:
     S3_ACCESS_KEY = S3_ACCESS_KEY
     S3_SECRET_ACCESS = S3_SECRET_ACCESS
     S3_REGION = S3_REGION
+    S3_BUCKET_NAME = S3_BUCKET_NAME
     DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
