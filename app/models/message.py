e1d72529284474403cc7d8bcc90c83e9ba23fbba
heunyam <tpwns072@gmail.com>
2021-01-25 18:50:34 +0900

    fix

--- app/models/message.py
+++ app/models/message.py
@@ -6,8 +6,15 @@ from app.models import Base
 class Message(Base):
     __tablename__ = 'messages'
 
-    user_id = Column(String(45), ForeignKey('users.id'), primary_key=True)
-    room_id = Column(Integer, ForeignKey('rooms.id'), primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    user_id = Column(String(45), ForeignKey('users.id'))
+    room_id = Column(Integer, ForeignKey('rooms.id'))
     content = Column(String(300), nullable=False)
     type = Column(String(10), nullable=False)
     created_at = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
+
+
+class MessageType:
+    message = "text"
+    photo = "png"
+    voice = "mp3"
