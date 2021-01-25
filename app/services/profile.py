4c2825b1c5edf94b93fefd56081c7d1d321dc16f
heunyam <tpwns072@gmail.com>
2021-01-25 18:50:56 +0900

    refactor: remove service.profile

--- app/services/profile.py
+++
@@ -1,6 +0,0 @@
-from app.models import s3
-from config.db import RemoteDBConfig
-
-
-def upload_image_to_s3(image, image_name):
-    s3.upload_fileobj(image, RemoteDBConfig.S3_BUCKET_NAME, image_name)
