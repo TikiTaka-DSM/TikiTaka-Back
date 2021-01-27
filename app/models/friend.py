5f75d1afdc417bf2f4439c6fac755368cec049b0
heunyam <tpwns072@gmail.com>
2021-01-28 06:19:49 +0900

    refactor: models to service

--- app/models/friend.py
+++ app/models/friend.py
@@ -9,18 +9,3 @@ class Friend(Base):
     user_id = Column(String(45), ForeignKey('users.id'), primary_key=True)
     friend_user_id = Column(String(45), ForeignKey('users.id'), primary_key=True)
     blocking_state = Column(Boolean, nullable=False, default=False)
-
-
-@catch_exception
-def get_friendship_data(owner_user_id):
-    friendships = session.query(Friend).filter(Friend.user_id == owner_user_id).all()
-
-    return friendships
-
-
-@catch_exception
-def get_friend_state(owner_user, other_user):
-    friendship = session.query(Friend).filter(Friend.user_id == owner_user).\
-                          filter(Friend.friend_user_id == other_user).first()
-
-    return friendship
