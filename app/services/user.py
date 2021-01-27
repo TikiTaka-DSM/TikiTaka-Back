83d7af31837d968dc054d9565cbd4682e164626c
heunyam <tpwns072@gmail.com>
2021-01-28 06:22:06 +0900

    refactor: models to service

---
+++ app/services/user.py
@@ -0,0 +1,34 @@
+from app.models import session, catch_exception
+from app.models.user import User, UserDefaultValue
+
+
+def insert_user(user_id, user_password, user_name):
+
+    user = User(id=user_id,
+                password=user_password,
+                name=user_name,
+                img=UserDefaultValue.image,
+                introduction=UserDefaultValue.introduction)
+
+    session.add(user)
+    session.commit()
+    session.close()
+
+
+@catch_exception
+def get_user_data_by_user_id(user_id):
+    user_data = session.query(User).filter(User.id == user_id).first()
+
+    return user_data
+
+
+@catch_exception
+def update_profile(user_id, user_data):
+    user = get_user_data_by_user_id(user_id)
+
+    user.name = user_data.name if user_data.name else user.name
+    user.img = user_data.img if user_data.img else user.img
+    user.introduction = user_data.status_message if user_data.status_message else user.introduction
+
+    session.commit()
+    session.close()
