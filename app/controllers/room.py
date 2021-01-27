fb9d3f91bd0a85407811ad09d1783691a4bc1bb8
heunyam <tpwns072@gmail.com>
2021-01-28 06:21:05 +0900

    fix: return room id instead of return 409

--- app/controllers/room.py
+++ app/controllers/room.py
@@ -1,26 +1,15 @@
-from flask import abort
+from app.services.member import get_room_id
+from app.services.room import insert_room
 
-from app.models.room import insert_room, get_room_data_by_users_id
 
+def create_new_chatting_room(owner_user_id, friend_user_id):
+    room_id = get_room_id(owner_user_id, friend_user_id)
 
-def create_new_chatting_room(owner_user, other_user):
-
-    if get_room_data_by_users_id(owner_user, other_user):
-        abort(409, "This room has already been created")
-
-    room_id = insert_room(owner_user, other_user)
+    if not room_id:
+        room_id = insert_room()
 
     return {
         "roomData": {
             "id": room_id
         }
     }
-
-
-# def get_chatting_rooms(owner_user):
-    # rooms = get_rooms_by_name(owner_user)
-
-
-
-
-
