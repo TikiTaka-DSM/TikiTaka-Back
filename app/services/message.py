9097b3e3cd22ba9ef0d309ca874e8f26826ded9f
heunyam <tpwns072@gmail.com>
2021-01-28 11:49:29 +0900

    feat: add message services

--- app/services/message.py
+++ app/services/message.py
@@ -1,7 +1,8 @@
 from app.models.message import Message
-from app.models import session
+from app.models import session, catch_exception
 
 
+@catch_exception
 def insert_message(user_id, room_id, content, type):
     message = Message(user_id=user_id,
                       room_id=room_id,
@@ -13,3 +14,18 @@ def insert_message(user_id, room_id, content, type):
     session.close()
 
     return message
+
+
+@catch_exception
+def get_latest_message(room_id, user_id):
+    return session.query(Message).\
+        filter(Message.room_id == room_id).\
+        filter(Message.user_id == user_id).\
+        order_by(Message.created_at.desc()).first()
+
+
+@catch_exception
+def get_messages(room_id):
+    return session.query(Message).\
+        filter(Message.room_id == room_id).\
+        order_by(Message.created_at).all()
