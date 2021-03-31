8ab040202017f5dc0c027095be908679c91c745b
heunyam <tpwns072@gmail.com>
2021-03-31 20:22:14 +0900

    fix: User password length 45 to 100

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(String(45), nullable=False, primary_key=True)
-    password = Column(String(45), nullable=False)
+    password = Column(String(100), nullable=False)
     name = Column(String(45), nullable=False)
     img = Column(String(100), nullable=True)
     introduction = Column(String(100), nullable=True)
