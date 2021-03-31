8c553b11ed51a41da8ba22b9ba3268e92cb25a1c
heunyam <tpwns072@gmail.com>
2021-03-31 20:20:09 +0900

    feat: Add decorater validation with request args

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,7 +2,7 @@ from functools import wraps
 from flask import request, abort
 
 
-def validate_JSON(model):
+def validate_json(model):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -10,9 +10,25 @@ def validate_JSON(model):
                 instance = model(getattr(request, 'json'))
                 instance.validate()
             except:
-                abort(400, "check JSON")
+                abort(400, "check JSON key and values")
 
             return func(*args, **kwargs)
         return wrapper
     return decorator
 
+
+def validate_args(model):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                instance = model(getattr(request, 'args'))
+                instance.validate()
+            except:
+                abort(400, "check args")
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
