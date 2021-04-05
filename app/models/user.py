36ca8363edbb292e3ec42a8792e5774e51930c45
heunyam <tpwns072@gmail.com>
2021-04-05 22:59:36 +0900

    refactor: User Model에서 server_default 지정

--- app/models/user.py
+++ app/models/user.py
@@ -3,16 +3,21 @@ from sqlalchemy import Column, String
 from app.models import Base
 
 
+class UserDefaultValue:
+    image = 'default.png'
+    introduction = ''
+
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(String(45), nullable=False, primary_key=True)
     password = Column(String(100), nullable=False)
     name = Column(String(45), nullable=False)
-    img = Column(String(100), nullable=True)
-    introduction = Column(String(100), nullable=True)
+    img = Column(String(100), nullable=True, server_default=UserDefaultValue.image)
+    introduction = Column(String(100), nullable=True, server_default=UserDefaultValue.introduction)
 
-
-class UserDefaultValue:
-    image = 'default.png'
-    introduction = ''
+    def __init__(self, id, password, name):
+        self.id = id
+        self.password = password
+        self.name = name
