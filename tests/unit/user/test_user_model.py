711dea8d8e9ca2c747ac14a8a7cbf5d6c661aefd
heunyam <tpwns072@gmail.com>
2021-04-30 10:10:42 +0900

    test:

--- tests/unit/user/test_user_model.py
+++ tests/unit/user/test_user_model.py
@@ -1,15 +1,13 @@
 import pytest
-from app.models.user import User
-from werkzeug.security import generate_password_hash
+from app.models.user import UserModel
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @pytest.fixture(scope='module')
 def new_user():
-    user = User(id="testuser1",
-                password=generate_password_hash("testuserpassword1"),
-                name="테스트1",
-                img="default.png",
-                introduction="")
+    user = UserModel(id="testuser1",
+                     password=generate_password_hash("testuserpassword1"),
+                     name="테스트1")
 
     return user
 
@@ -20,8 +18,7 @@ def test_new_user(new_user):
     WHEN a new User is created
     THEN check the email, password, name, img, introduction fields are defined correctly
     """
+    print(new_user)
     assert new_user.id == "testuser1"
     assert new_user.password != "testuserpassword1"
     assert new_user.name == "테스트1"
-    assert new_user.img == "default.png"
-    assert new_user.introduction == ""
