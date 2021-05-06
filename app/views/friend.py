654ff313b9a2ad5b6a31b001c6d1f5f6621a40e3
heunyam <tpwns072@gmail.com>
2021-05-06 15:18:14 +0900

    refactor: search by friend name API를 Friends API로 병합

--- app/views/friend.py
+++ app/views/friend.py
@@ -1,29 +1,39 @@
 from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from schematics import Model
+from schematics.types import IntType
 
 from app.controllers.friend import create_new_friend, get_friends, search_friend_by_user_id, search_friend_by_user_name, block_friend
 
 
 class Friends(Resource):
+    class Schema(Model):
+        user_id = IntType(
+            serialized_name='userId',
+            required=True
+        )
 
     @jwt_required
     def get(self):
         owner_user = get_jwt_identity()
 
-        return get_friends(owner_user)
+        if request.args.get('name'):
+            user_name = request.args['name']
+            return search_friend_by_user_name(owner_user, user_name)
+        else:
+            return get_friends(owner_user)
 
     @jwt_required
     def post(self):
         owner_user = get_jwt_identity()
-        user_id = request.json['friendId']
+        user_id = request.json['userId']
 
         return create_new_friend(owner_user, user_id)
 
 
 class Friend(Resource):
-    def get(self):
-        user_id = request.args['id']
+    def get(self, user_id):
 
         return search_friend_by_user_id(user_id)
 
@@ -32,13 +42,3 @@ class Friend(Resource):
         owner_user = get_jwt_identity()
 
         return block_friend(owner_user, user_id)
-
-
-class SearchFriendName(Resource):
-    @jwt_required
-    def get(self):
-        owner_user = get_jwt_identity()
-        user_name = request.args['name']
-
-        return search_friend_by_user_name(owner_user, user_name)
-
